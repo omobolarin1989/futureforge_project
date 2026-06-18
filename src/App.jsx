24e1d81aafb8420bf0f02809d65f1e29535175c0
omobolarin1989@gmail.com <omobolarin1989@gmail.com@gmail.com>
2026-06-18 22:13:29 +0100

    reg page error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import Landing from './page/landing'
+import Landing from "./page/landing";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RegistrationForm from './page/Registration-form'
-import Scroll from './page/scroll';
-import { useState } from 'react'
+import Scroll from "./page/scroll";
+import { useState } from "react";
 
-import './App.css'
-import CohortStory from './page/cohort-story';
-import Cohort1 from './page/cohort1';
-import TermsCondition from './page/terms';
-import PrivacyData from './page/privacy';
+import CohortStory from "./page/cohort-story";
+import Cohort1 from "./page/cohort1";
+import TermsCondition from "./page/terms";
+import PrivacyData from "./page/privacy";
+import RegistrationForm from "./page/Registration-form";
+import "./App.css";
 
 function App() {
-  
-
   return (
- <BrowserRouter>
- <Scroll />
+    <BrowserRouter>
+      <Scroll />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/registration-form" element={<RegistrationForm />} />
@@ -24,11 +22,9 @@ function App() {
         <Route path="/cohort1/track/:trackId" element={<Cohort1 />} />
         <Route path="/terms" element={<TermsCondition />} />
         <Route path="/privacy" element={<PrivacyData />} />
-       
-        
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
